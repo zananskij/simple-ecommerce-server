245d6c536b255382dd7fb6d9ec916399df167763
zananskij <zananskij@gmail.com>
2023-06-01 17:25:45 -0400

    stripe payment added & working

--- server.js
+++ server.js
@@ -1,25 +1,71 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 const app = express()
+app.use(cors())
 app.use(express.json())
+// app.use(cors({ origin: 'http://your-frontend-url' }));
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
-app.post("/create-payment-intent", async (req, res) => {
+// app.post("/create-checkout-session", async (req, res) => {
+//   const { items } = req.body
+//   console.log(items) // Log the items here to check their structure
+
+//   const line_items = products.map((product) => ({
+//     price_data: {
+//       currency: "usd",
+//       unit_amount: product.price * 100, // Stripe requires the price in cents
+//       product_data: {
+//         name: product.description, // you need to provide product name here
+//         description: product.specs,
+//         images: [product.img],
+//       },
+//     },
+//     quantity: product.quantity,
+//   }))
+
+//   const session = await stripe.checkout.sessions.create({
+//     payment_method_types: ["card"],
+//     line_items: lineItems,
+//     mode: "payment",
+//     success_url: "http://localhost:3000/success", // Replace with your success page
+//     cancel_url: "http://localhost:3000/cancel", // Replace with your cancel page
+//   })
+
+//   res.json({ sessionId: session.id })
+// })
+app.post("/create-checkout-session", async (req, res) => {
   const { items } = req.body
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: "usd",
+  console.log(items) // Log the items here to check their structure
+
+  const line_items = items.map((item) => ({
+    price_data: {
+      currency: "usd",
+      unit_amount: item.price * 100, // Stripe requires the price in cents
+      product_data: {
+        name: item.description, // you need to provide product name here
+        description: item.specs,
+        // images: [item.img],
+      },
+    },
+    quantity: item.quantity,
+  }))
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: line_items,
+    mode: "payment",
+    success_url: "https://localhost:3000/success", // Replace with your success page
+    cancel_url: "https://localhost:3000/cancel", // Replace with your cancel page
   })
 
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  })
+  res.json({ sessionId: session.id })
 })
-function calculateOrderAmount(items) {
-  return items.reduce((total, item) => total + item.price * 100 * item.quantity, 0)
-}
+
+app.get("/success", (req, res) => {
+  res.send("Payment was successful!")
+})
 
 app.listen(4242, () => console.log("Node server listening on port 4242!"))
 
@@ -30,3 +76,28 @@ app.listen(4242, () => console.log("Node server listening on port 4242!"))
 // ])
 //keep item information (name & price on server (ideally Database or jsonfile) that way you just send the ID from client to server and how many of each ID)
 // npm run devStart
+
+//   v1
+//   const lineItems = items.map((item) => ({
+//     price_data: {
+//       currency: "usd",
+//       product_data: {
+//         name: item.name,
+//       },
+//       unit_amount: item.price * 100, // Stripe needs the price in cents
+//     },
+//     quantity: item.quantity,
+//   }))
+//   v2
+//   const lineItems = items.map((item) => ({
+//     price_data: {
+//       currency: "usd",
+//       product_data: {
+//         name: item.name,
+//         description: item.description,
+//         images: [item.img],
+//       },
+//       unit_amount: item.price * 100, // Stripe needs the price in cents
+//     },
+//     quantity: item.quantity,
+//   }))
